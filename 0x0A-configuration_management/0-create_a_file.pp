a3f57cf3fbed8e07aebce8f26daf4fecfcad5a47
Brett Davis <brett.p.davis101@gmail.com>
2020-08-03 22:07:22 +0000

    Again shouldnt change anything, but in the resource this is the order they appear in

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 file { '/tmp/holberton':
   path    => '/tmp/holberton',
+  content => 'I love Puppet',
+  group   => 'www-data',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
 }
