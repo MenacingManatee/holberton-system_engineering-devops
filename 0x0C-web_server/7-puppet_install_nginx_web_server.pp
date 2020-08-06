cf15179071543a07395dfbc85b7172609885a25d
Brett Davis <brett.p.davis101@gmail.com>
2020-08-06 02:22:56 +0000

    TODO: Add 301 redirect for /redirect_me

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,1 +1,11 @@
 # Creates and configures a nginx webserver
+node '34.73.196.89' {
+  package { 'nginx':
+    ensure => installed,
+  }
+}
+file { '/var/www/html/index.nginx-debian.html':
+  path    => '/var/www/html/index.nginx-debian.html',
+  content => 'Holberton School',
+  mode    => '0744',
+}
