fcb711e348821187cabf8bf736ab7ece22899c79
Brett Davis <brett.p.davis101@gmail.com>
2020-09-17 07:15:28 +0000

    Noticed a weird error where sometimes a non-existant subreddit would still have 'data' in the json, so added an extra check to cover it

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+'''queries the Reddit API and returns the number of subscribers
+for a given subreddit'''
+import requests
+
+
+def number_of_subscribers(subreddit):
+    '''returns the number of subscribers, or 0 if invalid'''
+    user_agent = {'User-agent': 'User Agent 1.0'}
+    jsn = requests.get('https://reddit.com/r/{}/about.json'.format(subreddit),
+                       headers=user_agent)
+    data = jsn.json().get('data')
+    if data is None or data.get('subscribers') is None:
+        return 0
+    else:
+        return data.get('subscribers')
