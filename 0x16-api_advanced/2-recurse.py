73e144be8d3e65da6a064897970b63debdddddef
Brett Davis <brett.p.davis101@gmail.com>
2020-09-17 23:31:52 +0000

    Seems to work, but having trouble testing on my slow computer. Saving before reboot

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+'''recursive function that queries the Reddit API'''
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=''):
+    '''returns a list of all hot titles on a subreddit'''
+    print(len(hot_list))
+    u_a = {'User-agent': 'User Agent 1.0'}
+    if len(hot_list) == 0:
+        data = requests.get('https://reddit.com/r/{}/hot.json?limit=1'.
+                            format(subreddit), headers=u_a)
+        jsn = data.json().get('data')
+        if jsn is None or jsn == {}:
+            return None
+        hot_list.append(jsn.get('children')[0].get('data').
+               get('title'))
+        aft = jsn.get('after')
+        hot_list = recurse(subreddit, hot_list, aft)
+        return hot_list
+    else:
+        data = requests.get('https://reddit.com/r/{}/hot.json?limit=1&after={}'.
+                            format(subreddit, after), headers=u_a)
+        jsn = data.json().get('data')
+        if jsn is None or jsn == {}:
+            return hot_list
+        hot_list.append(jsn.get('children')[0].get('data').
+               get('title'))
+        aft = jsn.get('after')
+        hot_list = recurse(subreddit, hot_list, aft)
+        return hot_list
