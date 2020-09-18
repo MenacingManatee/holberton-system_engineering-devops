5628cde5799d50581b54a92add3614dd755263fe
Brett Davis <brett.p.davis101@gmail.com>
2020-09-18 04:43:11 +0000

    Removed limit to prevent autokilling early due to recursion depth

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,25 +6,33 @@ import requests
 def recurse(subreddit, hot_list=[], after=''):
     '''returns a list of all hot titles on a subreddit'''
     u_a = {'User-agent': 'User Agent 1.0'}
+    if len(hot_list) >= 2 and hot_list[-1] == hot_list[-2]:
+        del hot_list[-1]
+        del hot_list[-1]
+        if hot_list == []:
+            return None
+        return hot_list
     if len(hot_list) == 0:
         data = requests.get('https://reddit.com/r/{}/hot.json?limit=1'.
                             format(subreddit), headers=u_a)
         jsn = data.json().get('data')
-        if jsn is None or jsn == {}:
+        if jsn is None or jsn.get('children') is None:
             return None
         hot_list.append(jsn.get('children')[0].get('data').
-               get('title'))
+                        get('title'))
         aft = jsn.get('after')
         hot_list = recurse(subreddit, hot_list, aft)
         return hot_list
     else:
-        data = requests.get('https://reddit.com/r/{}/hot.json?limit=1&after={}'.
-                            format(subreddit, after), headers=u_a)
+        data = requests.get('https://reddit.com/r/{}/hot.json?after={}'
+                            .format(subreddit, after), headers=u_a)
         jsn = data.json().get('data')
-        if jsn is None or jsn == {}:
+        if jsn is None or jsn.get('children') is None:
             return hot_list
-        hot_list.append(jsn.get('children')[0].get('data').
-               get('title'))
+        for item in jsn.get('children'):
+            hot_list.append(item.get('data').get('title'))
+#        hot_list.append(jsn.get('children')[0].get('data').
+#                        get('title'))
         aft = jsn.get('after')
         hot_list = recurse(subreddit, hot_list, aft)
         return hot_list
