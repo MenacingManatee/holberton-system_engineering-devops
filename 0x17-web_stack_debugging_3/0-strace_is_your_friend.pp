97e0bf4a0e87639a89817e66660e5b6a8c4e910c
Brett Davis <brett.p.davis101@gmail.com>
2020-09-29 10:41:40 +0000

    Styling fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,6 +3,6 @@ file { '/var/www/html/wp-settings.php' :
   ensure => present,
 }
 exec { 'find and replace phpp' :
-  command    => 'cat /var/www/html/wp-settings.php | sed \'s/phpp/php/g\' | sudo tee /var/www/html/wp-settings.php',
-  provider   => 'shell',
+  command  => 'cat /var/www/html/wp-settings.php | sed \'s/phpp/php/g\' | sudo tee /var/www/html/wp-settings.php',
+  provider => 'shell',
 }
